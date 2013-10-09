233ed7bc43a7d229c2e5cf806500369e0982f981
g <g@g.(none)>
2013-10-09 18:10:57 -0400

    adding ledger

--- vmxCount.js
+++ vmxCount.js
@@ -1,7 +1,20 @@
 VMX.config.useMagicCanvas = true;
 VMX.callback = function(detections){
-  var mc = VMX.getMagicCanvas();
+  if (!mc = VMX.getMagicCanvas()){
+    return; //canvas not open 
+  };
+  var modelName = detections[0].cls;
+
   var mcc = mc.getContext('2d');
-  console.log(detections);
+
+  for(var x in detections){
+    if(detections[x].score > MIN_CONFIDENCE){
+      VMX.storage.ledger[modelName] = (x+1);
+    } else {
+      break;
+    }
+  }
+
+  console.log(VMX.storage.ledger);
 }
  
