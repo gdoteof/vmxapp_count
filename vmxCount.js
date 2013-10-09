8f5950293f0a096409c8176e82531f999267cc20
g <g@g.(none)>
2013-10-09 18:40:30 -0400

    fixing bad container

--- vmxCount.js
+++ vmxCount.js
@@ -5,25 +5,20 @@ VMX.storage.ledger = {}
 console.log("RE STARTING COUNTER");
 
 VMX.callback = function(detections){
-  console.log("eh..?");
   if (! ( mc = VMX.getMagicCanvas() ) ){
     return; //canvas not open 
   };
   var modelName = detections[0].cls;
-  console.log("ok...");
-/*
   var mcc = mc.getContext('2d');
-  console.log("VMX..",VMX);
 
   for(var x in detections){
     if(detections[x].score > MIN_CONFIDENCE){
-//      VMX.storage.ledger[modelName] = (x+1);
+      VMX.storage.ledger[modelName] = (x+1);
     } else {
       break;
     }
   }
 
   console.log(VMX.storage.ledger);
-  */
 }
  
