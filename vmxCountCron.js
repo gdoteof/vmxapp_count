ac42baac75855d44c8248629a9a01764940ff6df
g <g@g.(none)>
2013-10-09 22:17:04 -0400

    fc

--- vmxCountCron.js
+++ vmxCountCron.js
@@ -7,7 +7,7 @@ VMX.cron = function(){
     var diff = now - model.last_seen;
     console.log("its been ", diff, "since we've seen", model.name);
     if(now - model.last_seen > 2000){
-      delete VMX.storage[model];
+      delete VMX.storage.ledger[model];
     }
   }
 }
