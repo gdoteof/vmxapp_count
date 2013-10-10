bd6f19b0b249aeda9477ba54044315507ee6a168
g <g@g.(none)>
2013-10-10 00:22:32 -0400

    fc

--- vmxCountCron.js
+++ vmxCountCron.js
@@ -1,13 +1,16 @@
 console.log("ontopofcount");
+//Set params
 vmxparams.list_ui_params()[0].value=1000;
 vmxparams.list_ui_params()[0].current_value=1000;
+vmxparams.list_ui_params()[2].value=0;
+vmxparams.list_ui_params()[2].current_value=0;
 VMX.cron = function(){
   var now = new Date();
 
   for(var m in VMX.storage.ledger){
     model = VMX.storage.ledger[m];
     var diff = now - model.last_seen;
-    if(diff > 700){
+    if(diff > 400){
       VMX.storage.ledger[m].count = 0;
     }
   }
