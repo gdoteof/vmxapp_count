027a2a027c11a18b72c280a073a4fdc58aad90c2
g <g@g.(none)>
2013-10-09 22:16:10 -0400

    fc

--- vmxCountCron.js
+++ vmxCountCron.js
@@ -2,7 +2,8 @@ console.log("ontopofcount");
 VMX.cron = function(){
   var now = new Date();
 
-  for(var model in VMX.storage.ledger){
+  for(var m in VMX.storage.ledger){
+    model = VMX.storage.ledger[m];
     var diff = now - model.last_seen;
     console.log("its been ", diff, "since we've seen", model.name);
     if(now - model.last_seen > 2000){
