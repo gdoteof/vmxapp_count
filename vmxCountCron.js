b5b116897f9877d6e6c2bfd7618c12eb4b709f3e
g <g@g.(none)>
2013-10-10 21:37:58 -0400

    fc

--- vmxCountCron.js
+++ vmxCountCron.js
@@ -14,7 +14,7 @@ VMX.cron = function(){
   for(var m in VMX.storage.ledger){
     model = VMX.storage.ledger[m];
     var diff = now - model.last_seen;
-    if(diff > 1000){
+    if(diff > 2000){
       VMX.storage.ledger[m].count = 0;
     }
   }
